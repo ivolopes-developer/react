1b6ea8958d81ee810e618267b21263389ed041fe
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-19 16:53:40 +0000

    JSX Rules

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,24 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // component or function
+// function Greeting() {
+//   return (
+//     <div> or section
+//       <div>
+//         <h3>Hello People</h3>
+//         <ul>
+//           <li>
+//             <a href='#'>Hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//     </div>
+//   );
+// }
 function Greeting() {
   return (
-    <div>
-      <h2>My First Component</h2>
-    </div>
+    <React.Fragment>
+      <div className='someValue'>
+        <h3>Hello People</h3>
+        <ul>
+          <li>
+            <a href='#helloworld'>Hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>hello world</h2>
+      <input type='text' name='' id='' />
+    </React.Fragment>
   );
 }
-// method 2
-// function Greeting() {
-//   return React.createElement("h2", {}, "hello world");
-// }
-// const Greeting = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h2", {}, "hello world")
-//   );
-// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
