6bb37237a5f199d49f1d2eb3d7a46cc04b85a6a5
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-19 17:06:54 +0000

    Nest Components

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,38 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// component or function
-// function Greeting() {
-//   return (
-//     <div> or section
-//       <div>
-//         <h3>Hello People</h3>
-//         <ul>
-//           <li>
-//             <a href='#'>Hello world</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <h2>hello world</h2>
-//     </div>
-//   );
-// }
 function Greeting() {
   return (
     <React.Fragment>
-      <div className='someValue'>
-        <h3>Hello People</h3>
-        <ul>
-          <li>
-            <a href='#helloworld'>Hello world</a>
-          </li>
-        </ul>
-      </div>
-      <h2>hello world</h2>
-      <input type='text' name='' id='' />
+      <Person></Person>
+      <Message></Message>
     </React.Fragment>
   );
 }
+
+const Person = () => <h2>John Doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
