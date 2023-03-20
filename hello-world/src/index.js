271ff2aabe3c29ca4e445148e5e93455e742f251
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-20 21:03:57 +0000

    props - basic setup

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,29 +4,28 @@ import ReactDOM from "react-dom/client";
 //import files
 import "./Styles/style.css";
 
+const author = "Benjamin Hall";
+const title = "A War Reporter's Mission to Make It Home";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg";
+
 //amazon best selling books - START ----------------------------------------------------------------
 function BookList() {
   return (
     <section className='booklist'>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
+      <Book img={img} title={title} author={author}></Book>
+      <Book img={img} title={title} author={author}></Book>
     </section>
   );
 }
 
-const author = "Benjamin Hall";
-const Book = () => {
-  const title = "A War Reporter's Mission to Make It Home";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg'
-        alt='book1'
-      />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
