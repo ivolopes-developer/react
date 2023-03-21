6268adef7ec124efe0075862a3cb95c0d777e781
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-21 17:10:32 +0000

    LISTS and KEY PROP
    on lists Key id I can use "books.map((book, index)" (index as parameter) but is not recommende if I'm not sure that List will never change.

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,39 +4,51 @@ import ReactDOM from "react-dom/client";
 //import files
 import "./Styles/style.css";
 
-const firstBook = {
-  author: "Benjamin Hall",
-  title: "A War Reporter's Mission to Make It Home",
-  img: "https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg",
-};
-
-const secondBook = {
-  author: "James Clear",
-  title:
-    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
+//array of books
+const books = [
+  {
+    author: "Benjamin Hall",
+    title: "A War Reporter's Mission to Make It Home",
+    img: "https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    id: 2,
+  },
+];
 
 //amazon best selling books - START ----------------------------------------------------------------
 function BookList() {
+  // return (
+  //   <section className='booklist'>
+  //     <Book
+  //       img={books[0].img}
+  //       title={books[0].title}
+  //       author={books[0].author}
+  //     ></Book>
+  //     <Book
+  //       img={books[1].img}
+  //       title={books[1].title}
+  //       author={books[1].author}
+  //     ></Book>
+  //   </section>
+  // );
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      ></Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      ></Book>
+      {books.map((book) => {
+        const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = (/* props */ { img, title, author }) => {
-  /* const { img, title, author } = props; */
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt={title} />
