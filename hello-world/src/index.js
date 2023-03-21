86e14af33ae9043b884f8e6114de3de8f8f87ae1
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-21 15:16:14 +0000

    props

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -4,17 +4,33 @@ import ReactDOM from "react-dom/client";
 //import files
 import "./Styles/style.css";
 
-const author = "Benjamin Hall";
-const title = "A War Reporter's Mission to Make It Home";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg";
+const firstBook = {
+  author: "Benjamin Hall",
+  title: "A War Reporter's Mission to Make It Home",
+  img: "https://images-na.ssl-images-amazon.com/images/I/713F+ivM9NL._AC_UL600_SR600,400_.jpg",
+};
+
+const secondBook = {
+  author: "James Clear",
+  title:
+    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  img: "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
 
 //amazon best selling books - START ----------------------------------------------------------------
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={img} title={title} author={author}></Book>
-      <Book img={img} title={title} author={author}></Book>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      ></Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      ></Book>
     </section>
   );
 }
