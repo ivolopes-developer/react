8d7457e2879cee07fd70b67c27bb765045ea82da
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-23 21:32:59 +0000

    Functions inside components
    lel GG

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -21,54 +21,53 @@ const books = [
   },
 ];
 
-const EventExamples = () => {
-  // const handleFormInput = (e) => {
-  //   console.log(e.target);
-  //   console.log(e.target.name);
-  //   console.log(e.target.value);
-  //   console.log("handle form input");
-  // };
+// const EventExamples = () => {
+//   // const handleFormInput = (e) => {
+//   //   console.log(e.target);
+//   //   console.log(e.target.name);
+//   //   console.log(e.target.value);
+//   //   console.log("handle form input");
+//   // };
 
-  // const handleButtonClick = () => {
-  //   alert("handle button click");
-  // };
+//   // const handleButtonClick = () => {
+//   //   alert("handle button click");
+//   // };
 
-  // const handleFormSubmission = (e) => {
-  //   e.preventDefault();
-  //   console.log("form submission done");
-  // };
+//   // const handleFormSubmission = (e) => {
+//   //   e.preventDefault();
+//   //   console.log("form submission done");
+//   // };
 
-  return (
-    <React.Fragment>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type='text'
-          name='product'
-          onChange={(e) => console.log(e.target.value)}
-          style={{ margin: "1rem 0" }}
-        />
-        <div>
-          <button
-            type='button'
-            /* onClick={handleFormSubmission} */ onClick={() =>
-              console.log("click me")
-            }
-          >
-            Form Submit
-          </button>
-        </div>
-      </form>
-      {/* <button onClick={handleButtonClick}>Click Me</button> */}
-    </React.Fragment>
-  );
-};
+//   return (
+//     <React.Fragment>
+//       <form>
+//         <h2>Typical Form</h2>
+//         <input
+//           type='text'
+//           name='product'
+//           onChange={(e) => console.log(e.target.value)}
+//           style={{ margin: "1rem 0" }}
+//         />
+//         <div>
+//           <button
+//             type='button'
+//             /* onClick={handleFormSubmission} */ onClick={() =>
+//               console.log("click me")
+//             }
+//           >
+//             Form Submit
+//           </button>
+//         </div>
+//       </form>
+//       {/* <button onClick={handleButtonClick}>Click Me</button> */}
+//     </React.Fragment>
+//   );
+// };
 
 //amazon best selling books - START ----------------------------------------------------------------
 function BookList() {
   return (
     <section className='booklist'>
-      <EventExamples></EventExamples>
       {books.map((book) => {
         return <Book {...book} key={book.id}></Book>;
       })}
@@ -78,10 +77,15 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const displayTitle = () => {
+    console.log(title);
+  };
+
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={displayTitle}>Click me here omg</button>
       <h4>{author}</h4>
     </article>
   );
