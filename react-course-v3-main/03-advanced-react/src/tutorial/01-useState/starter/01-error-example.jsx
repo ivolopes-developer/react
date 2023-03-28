2e8e7cf78125ddb63123ae11a487797b3a2ff633
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-28 18:20:44 +0100

    useState Basics

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
+import React from "react";
+
 const ErrorExample = () => {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  const handleClick_increase = () => {
+    setCount(count + 1);
+  };
+
+  const handleClick_decrease = () => {
+    setCount(count - 1);
+  };
 
   return (
-    <div>
-      <h2>{count}</h2>
-      <button className='btn' type='button' onClick={() => count++}>
-        Increase
-      </button>
-    </div>
+    <React.Fragment>
+      <div>
+        <h2>{count}</h2>
+        <button className='btn' type='button' onClick={handleClick_increase}>
+          Increase
+        </button>
+      </div>
+      <div style={{ margin: 10 }}>
+        <button className='btn' type='button' onClick={handleClick_decrease}>
+          Decrease
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h4>You Clicked {count} times</h4>
+      <button type='button' className='btn' onClick={handleClick}>
+        Click Here
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
