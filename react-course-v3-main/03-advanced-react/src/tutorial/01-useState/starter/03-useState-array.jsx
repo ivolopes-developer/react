108fe7af283d3753c7ffcebac82d51952371e715
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-29 18:42:45 +0100

    completing array example

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,52 @@
+import { data } from "../../../data";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  /**
+   * It takes an id as an argument, filters out the person with that id from the people array, and then
+   * sets the people array to the new array
+   */
+  const removeItem = (id) => {
+    console.log(id);
+    // const newPeople = people.filter((person) => person.id !== id);
+
+    // setPeople(newPeople);
+    setPeople(people.filter((person) => person.id !== id));
+  };
+
+  // const clearAllItems = () => {
+  //   setPeople([]);
+  // };
+
+  return (
+    <React.Fragment>
+      <div>
+        {people.map((person) => {
+          const { id, name } = person;
+          // console.log(person);
+          return (
+            <div key={id}>
+              <h4>{name}</h4>
+              <button
+                onClick={() => removeItem(id)}
+                style={{ marginBottom: 30 }}
+                type='button'
+              >
+                delete name
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div style={{ marginTop: 100 }}>
+        <button className='btn' onClick={() => setPeople([])} type='button'>
+          delete all items
+        </button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
