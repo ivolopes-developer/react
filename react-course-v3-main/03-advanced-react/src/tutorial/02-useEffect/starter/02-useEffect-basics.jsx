9d29e4f11578a36e76cf883b183a0897aa1d7398
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-03-30 18:20:26 +0100

    UseEffect Multiple Effects

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -2,21 +2,26 @@ import { useState, useEffect } from "react";
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
-  const sayHello = () => {
-    console.log("hello there");
-  };
+  const [secValue, setSecValue] = useState(0);
 
-  sayHello();
-
+  /* A hook that is called after every render. */
   useEffect(() => {
     console.log("hello from useEffect");
-  }, []);
+  }, [value, secValue]);
+
+  // useEffect(() => {
+  //   console.log("hello from useEffect (2nd value)");
+  // }, [secValue]);
 
   return (
     <div>
       <h1>value : {value}</h1>
       <button className='btn' onClick={() => setValue(value + 1)}>
-        click me
+        increase 1st value
+      </button>
+      <h1>second value : {secValue}</h1>
+      <button className='btn' onClick={() => setSecValue(secValue + 1)}>
+        increase 2nd value
       </button>
     </div>
   );
