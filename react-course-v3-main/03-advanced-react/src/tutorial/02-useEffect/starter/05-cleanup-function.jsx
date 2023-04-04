8826c3120fb3946f42af65bb3179dc6bbfa72c80
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-04 19:22:34 +0100

    cleanUp function + event listener
    yo

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      <button className='btn' onClick={() => setToggle(!toggle)}>
+        Toggle Component
+      </button>
+      {toggle && <RandomComponent></RandomComponent>}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    const someFunc = () => {
+      //someLogic
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
+  }, []);
+
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
