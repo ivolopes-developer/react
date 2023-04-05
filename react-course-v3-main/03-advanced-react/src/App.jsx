dbb4229d9aa5f1b67f67e49dd8ec450033d3eaa0
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-05 18:42:33 +0100

    Leverage JS

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -2,13 +2,14 @@ import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 import { Home, About } from "./tutorial/04-project-structure/starter/Pages"; //importing pages
 import Example from "./tutorial/04-project-structure/starter/Example";
+import List from "./tutorial/05-leverage-javascript/starter/List";
 // import Final from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
   return (
     <div className='container'>
-      <Starter></Starter>
-      {/* <Final></Final> */}
+      <h2>Leverage JS</h2>
+      <List></List>
     </div>
   );
 }
