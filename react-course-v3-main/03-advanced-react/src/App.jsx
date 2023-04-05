bb8f16d77ccff587e39203b571e55986058a7dcf
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-05 17:37:29 +0100

    folder setup

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,11 +1,15 @@
 import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 // import Final from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
   return (
+    // <div className='container'>
+    //   <Starter></Starter>
+    //   {/* <Final></Final> */}
+    // </div>
     <div className='container'>
-      <Starter></Starter>
-      {/* <Final></Final> */}
+      <Navbar></Navbar>
     </div>
   );
 }
