e86215d3d0002f80358bfca9b8993f36d371dd1e
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-05 17:53:28 +0100

    Export Group

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,15 +1,14 @@
 import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages"; //importing pages
+import Example from "./tutorial/04-project-structure/starter/Example";
 // import Final from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
   return (
-    // <div className='container'>
-    //   <Starter></Starter>
-    //   {/* <Final></Final> */}
-    // </div>
     <div className='container'>
-      <Navbar></Navbar>
+      <Starter></Starter>
+      {/* <Final></Final> */}
     </div>
   );
 }
