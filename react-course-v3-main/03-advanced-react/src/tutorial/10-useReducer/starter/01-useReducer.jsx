b9520db0381f6ba502acf9d2888f7af38b33c5d7
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-10 20:19:46 +0100

    useReducer : REMOVE_ITEM

--- react-course-v3-main/03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -23,7 +23,14 @@ const reducer = (state, action) => {
   } else if (action.type === RESET_LIST) {
     return { ...state, people: data };
   } else if (action.type === REMOVE_ITEM) {
-    return {};
+    /* `let newPeople = people.filter((person) => person.id !== id)` is filtering out the person with
+    the specified `id` from the `people` array and creating a new array `newPeople` that does not
+    include that person. This is used in the `REMOVE_ITEM` case of the `reducer` function to update
+    the state with the new array of people. */
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { ...state, people: newPeople };
   }
 
   // return state;
@@ -41,7 +48,7 @@ const ReducerBasics = () => {
   const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-    dispatch({ type: REMOVE_ITEM, id: id });
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
   };
