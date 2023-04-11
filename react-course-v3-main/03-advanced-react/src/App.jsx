7d29bfd0c9c283b6d43c7c9ef42a3b671b2459c4
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-11 22:17:35 +0100

    React.memo()

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 // import Starter from "./tutorial/09-context-api/starter/Navbar";
-import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
+import Starter from "./tutorial/11-performance/starter/03-hooks";
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 import { Home, About } from "./tutorial/04-project-structure/starter/Pages"; //importing pages
 import Example from "./tutorial/04-project-structure/starter/Example";
@@ -8,11 +8,11 @@ import React from "react";
 // import Final from "./tutorial/01-useState/starter/01-error-example";
 
 function App() {
-  return (
-    <div className='container'>
-      <Starter></Starter>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<Starter></Starter>
+		</div>
+	);
 }
 
 export default App;
--- react-course-v3-main/03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,21 +1,22 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
-const LowerState = () => {
-  const [people, setPeople] = useState(data);
-  const [count, setCount] = useState(0);
+import { useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
 
-  return (
-    <section>
-      <button
-        className='btn'
-        onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
-      >
-        count {count}
-      </button>
-      <List people={people} />
-    </section>
-  );
+const LowerState = () => {
+	const [people, setPeople] = useState(data);
+	const [count, setCount] = useState(0);
+
+	return (
+		<section>
+			<button
+				className='btn'
+				onClick={() => setCount(count + 1)}
+				style={{ marginBottom: "1rem" }}
+			>
+				count {count}
+			</button>
+			<List people={people} />
+		</section>
+	);
 };
 export default LowerState;
