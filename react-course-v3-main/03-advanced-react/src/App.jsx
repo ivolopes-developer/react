7f84712ec60cf8a0b12b6959692562b8774f103f
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-12 14:44:15 +0100

    useTransition

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 // import Starter from "./tutorial/09-context-api/starter/Navbar";
-import Starter from "./tutorial/11-performance/starter/03-hooks";
+import Starter from "./tutorial/11-performance/starter/04-react-18";
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 import { Home, About } from "./tutorial/04-project-structure/starter/Pages"; //importing pages
 import Example from "./tutorial/04-project-structure/starter/Example";
