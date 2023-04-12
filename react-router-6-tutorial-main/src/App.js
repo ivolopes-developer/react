743b35ebdb6182d37e80618e560823e4097ca4ee
Typhon88 <67379460+ivolopes-developer@users.noreply.github.com>
2023-04-12 16:39:30 +0100

    Nested Routes

--- react-router-6-tutorial-main/src/App.js
+++ react-router-6-tutorial-main/src/App.js
@@ -7,24 +7,13 @@ import Error from "./pages/Error";
 function App() {
 	return (
 		<BrowserRouter>
-			<nav>
-				<Link style={{ marginRight: "20px" }} to='/'>
-					Home
-				</Link>
-				<Link style={{ marginRight: "20px" }} to='about'>
-					About
-				</Link>
-				<Link style={{ marginRight: "20px" }} to='products'>
-					Products
-				</Link>
-			</nav>
 			<Routes>
-				<Route path='/' element={<Home></Home>}></Route>
-				<Route path='about' element={<About></About>}></Route>
-				<Route path='products' element={<Products></Products>}></Route>
-				<Route path='*' element={<Error></Error>}></Route>
+				<Route path='/' element={<Home></Home>}>
+					<Route path='about' element={<About></About>}></Route>
+					<Route path='products' element={<Products></Products>}></Route>
+					<Route path='*' element={<Error></Error>}></Route>
+				</Route>
 			</Routes>
-			<footer>This is a footer</footer>
 		</BrowserRouter>
 	);
 }
